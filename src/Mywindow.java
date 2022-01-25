a898f748078cb78c8c96ed14e09f855151f7e76a
Tim Levytskyi <tlevickij@gmail.com>
2022-01-25 18:38:26 +0200

    First comiit

---
+++ src/Mywindow.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Mywindow extends JFrame {
+    public Mywindow() {
+        super("MyWindow");
+        setSize(300, 100);
+        setLocation(200, 200);
+        setLayout(null);
+
+        setVisible(true);
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+class main{
+    public static void main (String[] a){
+        Mywindow window = new Mywindow();
+    }
+}
+
